ba430cd87c7118e0d3444cf33a4bf28bf4a12a41
Moises Muñiz <uo237351@uniovi.es>
2018-02-22 23:55:17 +0100

    cinta_v.1

--- Assets/Scripts/MoveAlongTheCinta.cs
+++ Assets/Scripts/MoveAlongTheCinta.cs
@@ -2,33 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class MoveAlongTheCinta : MonoBehaviour {
 
-    public Vector3 rotation;
-    List<GameObject> thingsToMove= new List<GameObject>();
-    public float velocity=1;
+
+    private Vector3[] vert;
+    public float velocity=0.1f;
+    private IDictionary<GameObject,int> objs= new Dictionary<GameObject, int>(); // Objeto y vertice actual
     // Use this for initialization
     void Start () {
-
+        vert = GetComponent<MeshFilter>().mesh.vertices;
+        assingMe(GameObject.FindGameObjectWithTag("Player"));
     }
 	
 	// Update is called once per frame
-	void Update () {
-       // Debug.DrawRay(this.transform.position, rotation.normalized, Color.red);
-        foreach(GameObject obj in thingsToMove)
-        {
-            //obj.transform.position += rotation.normalized*10;
-            obj.GetComponent<Rigidbody>().velocity=(rotation.normalized* velocity);
+	void FixedUpdate () {
+        List<GameObject> toInc = new List<GameObject>();
+        foreach (GameObject gm in objs.Keys) {
+            Vector3 vertice = transform.TransformPoint(vert[objs[gm]]);
+            float distSqr = Vector3.Distance( gm.transform.position, vertice);
+            gm.transform.position = Vector3.MoveTowards(gm.transform.position, vertice, velocity);
+            if (distSqr < 0.1f)
+            {
+                
+                    toInc.Add(gm);
+            }
         }
+        foreach(GameObject gm in toInc)
+        {
+            if (objs[gm] >= vert.Length - 1)
+            {
+                objs[gm] = 0;
+            }
+            else
+                objs[gm]++;
+            Debug.Log(objs[gm]);
+        }
+
     }
 
-    void OnTriggerEnter(Collider other)
+    public void assingMe(GameObject gm)
     {
-        thingsToMove.Add(other.gameObject);
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        thingsToMove.Remove(other.gameObject);
+        float minDistanceSqr = Mathf.Infinity;
+        int nearestVertex = -1;
+        for(int i=0; i< vert.Length; i++)
+        {
+            float distSqr = Vector3.Distance(gm.transform.position, transform.TransformPoint(vert[i]));
+            if (distSqr < minDistanceSqr)
+            {
+                minDistanceSqr = distSqr;
+                nearestVertex = i;
+            }
+        }
+        objs.Add(gm, nearestVertex);
     }
 }
