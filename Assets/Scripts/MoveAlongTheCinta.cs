c57b6e03b2bfde0d01d39fc41ba31ca3c278cd10
Moises Muñiz <uo237351@uniovi.es>
2018-02-22 16:11:14 +0100

    cinta_v.01

---
+++ Assets/Scripts/MoveAlongTheCinta.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAlongTheCinta : MonoBehaviour {
+
+    public Vector3 rotation;
+    List<GameObject> thingsToMove= new List<GameObject>();
+    public float velocity=1;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // Debug.DrawRay(this.transform.position, rotation.normalized, Color.red);
+        foreach(GameObject obj in thingsToMove)
+        {
+            //obj.transform.position += rotation.normalized*10;
+            obj.GetComponent<Rigidbody>().velocity=(rotation.normalized* velocity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        thingsToMove.Add(other.gameObject);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        thingsToMove.Remove(other.gameObject);
+    }
+}
