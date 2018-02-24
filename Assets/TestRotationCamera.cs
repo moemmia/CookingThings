bef370d5280dcd18c2ecc5b70f28fdd6f8de8d3e
Moises Muñiz <uo237351@uniovi.es>
2018-02-24 21:35:29 +0100

    Luces camara accion

---
+++ Assets/TestRotationCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotationCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(this.transform.up,0.1f);
+	}
+}
