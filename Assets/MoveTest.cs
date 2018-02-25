9afad423a7e7a760e777ed9cd2895c5b4bab7da4
moemmia <moises.muniz.mangas@gmail.com>
2018-02-25 13:25:55 +0100

    player movement test

---
+++ Assets/MoveTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            this.GetComponent<CharacterController>().Move(this.transform.forward*Input.GetAxis("Vertical")*0.1f);
+            this.GetComponent<CharacterController>().Move(this.transform.right * Input.GetAxis("Horizontal") * 0.1f);
+    }
+}
