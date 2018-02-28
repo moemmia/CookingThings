33fe53a717ddbd99f295980f36b1a5505f681791
moemmia <moises.muniz.mangas@gmail.com>
2018-02-28 19:36:38 +0100

    mouse test

---
+++ Assets/Scripts/jugador/Mouse/Mouse_Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mouse_Move : MonoBehaviour {
+    NavMeshAgent agent;
+    public GameObject animator;
+    public GameObject showWere;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        if (agent.hasPath && agent.remainingDistance > float.Epsilon)
+        {
+            if (!animator.GetComponent<Animator>().GetBool("walking"))
+                animator.GetComponent<Animator>().SetBool("walking", true);
+            showWere.SetActive(true);
+            showWere.transform.position= agent.destination;
+        }
+        else
+        {
+            if (animator.GetComponent<Animator>().GetBool("walking"))
+                animator.GetComponent<Animator>().SetBool("walking", false);
+            showWere.SetActive(false);
+        }
+
+    }
+
+    
+}
