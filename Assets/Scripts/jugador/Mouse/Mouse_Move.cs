14ebdec1d183997e21c79143d80fec087506945e
moemmia <moises.muniz.mangas@gmail.com>
2018-03-01 12:46:11 +0100

    doble click

--- Assets/Scripts/jugador/Mouse/Mouse_Move.cs
+++ Assets/Scripts/jugador/Mouse/Mouse_Move.cs
@@ -7,6 +7,7 @@ public class Mouse_Move : MonoBehaviour {
     NavMeshAgent agent;
     public GameObject animator;
     public GameObject showWere;
+    private float lastClickTime;
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -14,14 +15,29 @@ public class Mouse_Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        float catchTime =  0.3f;
+        if (Input.GetButtonDown("Mouse0"))
         {
             RaycastHit hit;
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
+                if (Time.time - lastClickTime < catchTime && Vector3.Distance( agent.destination, hit.point)<0.3f)
+                {
+                    //double click
+                    animator.GetComponent<Animator>().SetFloat("vel", 4f);
+                    agent.speed = 10f;
+                }
+                else
+                {
+                    //normal click
+                    animator.GetComponent<Animator>().SetFloat("vel", 2f);
+                    agent.speed = 5f;
+                }
                 agent.destination = hit.point;
             }
+            lastClickTime = Time.time;
+            
         }
         if (agent.hasPath && agent.remainingDistance > float.Epsilon)
         {
